eba8c1abde6f0b930aac2392598b660bb55b9f04
SANTYXGT <davidsantiagoguerrerotolosa@gmail.com>
2024-04-05 16:10:03 -0500

    Segundo commit
    se ingreso a la api de covid

--- DatosApi/inventario/api.py
+++ DatosApi/inventario/api.py
@@ -1,24 +1,13 @@
 from urllib.request import urlopen
 import json
-import urllib.parse  # Importa el módulo para codificar la URL
+from django.http import JsonResponse  # Importa JsonResponse para devolver datos JSON
 
-def ConsumirApi():
-    base_url = "https://gateway.marvel.com:443/v1/public/characters"
-    params = {
-        "name": "Hulk",
-        "apikey": "07ae1840831537496180cbbe6a6fc3dd",
-        "hash": "9956ccc80ee4192f80b9e91d6797ba69",
-        "ts": "1"
-    }
-    # Codifica los parámetros de la URL
-    encoded_params = urllib.parse.urlencode(params)
-    url = f"{base_url}?{encoded_params}"
+def ConsumirApi(request):
+    url = "https://api.covidtracking.com/v2/states/ca/2021-01-10.json"
 
     try:
         response = urlopen(url)
         data = json.loads(response.read())
-        return data
+        return data  # Devuelve los datos como un diccionario
     except Exception as e:
-        return print(f"Error al hacer la solicitud: {e}")
-
-ConsumirApi()
+        return {"error": f"Error al hacer la solicitud: {e}"}
--- DatosApi/inventario/views.py
+++ DatosApi/inventario/views.py
@@ -3,8 +3,6 @@ from inventario.api import ConsumirApi
 # Create your views here.
 
 def inicio(request):
-    try:
-        render(request,"invenatio/index.html", ConsumirApi)
-    except Exception as e:
-        render(request,"inventario/index.html")
+    context = ConsumirApi(request)
+    return render(request, "inventario/index.html", context)
     
